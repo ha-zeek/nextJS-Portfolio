aa0678c5fc0d31508541bcc60a366eff9cc18a15
Haziq Luqman <hazeek@Haziqs-MacBook-Air.local>
2023-12-06 16:32:43 +0800

    fix some issues

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -93,7 +93,7 @@ const TAB_DATA = [
 			<ul>
 				<li>
 					<span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-						Taylor's University
+						Taylors University
 					</span>
 				</li>
 				<li>
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -41,10 +41,10 @@ const EmailSection = () => {
 			className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-6"
 			id="contact">
 			<div>
-				<h5 className="text-xl font-bold text-white my-2"> Let's Connect</h5>
+				<h5 className="text-xl font-bold text-white my-2"> Lets Connect</h5>
 				<p className="text-gray-200 mb-4 max-w-md">
-					I'm currently looking for new opportunities, my inbox is always open.
-					Whether you have a question or just want to say hi, I'll try my best
+					I am currently looking for new opportunities, my inbox is always open.
+					Whether you have a question or just want to say hi, I will try my best
 					to get back to you!
 				</p>
 				<div className="socials flex flex-row gap-4">
